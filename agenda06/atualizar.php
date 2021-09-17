086438a78f7efa204432cbd281bd6e0cad7edf14
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2021-09-17 16:49:12 -0300

    Update atualizar.php

--- agenda06/atualizar.php
+++ agenda06/atualizar.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>Atualizar - MYSQLI</title>
+  <title>Atualizar - MYSQLI Teste</title>
 </head>
 
 <body>
